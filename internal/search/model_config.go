df67c19a3084ad3c68c994958724be81c855a3fc
Adam Shannon <adamkshannon@gmail.com>
2025-07-15 15:39:50 -0500

    search: set WAL mode on sqlite with configurable busy_timeout

--- internal/search/model_config.go
+++ internal/search/model_config.go
@@ -1,13 +1,23 @@
 package search
 
 import (
+	"time"
+
 	"github.com/moov-io/ach/cmd/achcli/describe/mask"
 )
 
 type Config struct {
-	SqliteFilepath string
+	Sqlite *SqliteConfig
 
 	BackgroundPrepare bool
 
 	AchMasking mask.Options
+}
+
+type SqliteConfig struct {
+	Directory   string
+	BusyTimeout time.Duration
+
+	MaxOpenConns int
+	MaxIdleConns int
 }
